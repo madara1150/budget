adbb9f463fcb7593f250e478966076cec5409e20
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2025-03-09 15:10:39 +0700

    [FiX] bug (#8)

--- budget_plan_ui/static/src/components/revenue_budget/revenue_budget.js
+++ budget_plan_ui/static/src/components/revenue_budget/revenue_budget.js
@@ -2,7 +2,13 @@
 
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
-import { Component, useState, onWillStart } from "@odoo/owl";
+import {
+  Component,
+  useState,
+  onWillStart,
+  useEnv,
+  onWillUnmount,
+} from "@odoo/owl";
 import { Budget_control_panel } from "../budget_control_panel/budget_control_panel";
 import { NoteEditor } from "../note_editor/note_editor";
 
@@ -32,13 +38,24 @@ export class Revenue_budget extends Component {
       toggle: {
         note: {},
       },
+      formated: {
+        amount: {},
+      },
     });
-
     onWillStart(async () => {
       await this.fetchData();
       await this.mergeData();
       await this.generateState();
     });
+  }
+
+  formattedAmount(value) {
+    return new Intl.NumberFormat("en-US").format(value);
+  }
+
+  parseNumber(value) {
+    if (!value) return 0;
+    return parseInt(value.toString().replace(/,/g, ""), 10) || 0;
   }
 
   updateNote = async (note, data) => {
@@ -74,7 +91,6 @@ export class Revenue_budget extends Component {
         }
       );
     this.state.budget_template_line.budget_template_merge = mergeData;
-    console.log(mergeData);
   }
 
   async generateState() {
@@ -84,6 +100,8 @@ export class Revenue_budget extends Component {
           this.state.budget_plan_line.budget_plan_line_amount[
             `${data.plan_line.id}-${data.id}`
           ] = data.plan_line.amount;
+          this.state.formated.amount[`${data.plan_line.id}-${data.id}`] =
+            this.formattedAmount(data.plan_line.amount);
           this.state.budget_plan_line.budget_plan_line_note[
             `${data.plan_line.id}-${data.id}`
           ] = data.plan_line.note;
@@ -99,13 +117,18 @@ export class Revenue_budget extends Component {
   }
 
   onBlurSavePlan = async (pos) => {
-    await this.orm.write("budget.plan.line", [pos.plan_line.id], {
+    const data = await this.orm.write("budget.plan.line", [pos.plan_line.id], {
       amount:
-        this.state.budget_plan_line.budget_plan_line_amount[
-          `${pos.plan_line.id}-${pos.id}`
-        ],
+      this.parseNumber(
+        this.state.formated.amount[`${pos.plan_line.id}-${pos.id}`]
+      ),
     });
-    await this.fetchData();
+    this.state.formated.amount[`${pos.plan_line.id}-${pos.id}`] =
+      this.formattedAmount(
+        this.state.formated.amount[`${pos.plan_line.id}-${pos.id}`]
+      );
+      await this.fetchData()
+      await this.generateState()
   };
 
   onBlurSaveCreate = async (pos) => {
@@ -114,14 +137,14 @@ export class Revenue_budget extends Component {
         plan_id: this.state.budget_plan.budget_plan_data.id,
         template_line_id: pos.id,
         amount:
-          this.state.budget_plan_line.budget_plan_line_amount[
-            `${pos.code}-${pos.id}`
-          ],
+        this.parseNumber(
+          this.state.formated.amount[`${pos.code}-${pos.id}`]
+        ),
       },
     ]);
-    await this.fetchData();
-    await this.mergeData();
-    await this.generateState();
+    this.state.formated.amount[`${pos.code}-${pos.id}`] = this.formattedAmount(
+      this.state.formated.amount[`${pos.code}-${pos.id}`]
+    );
   };
 
   async fetchData() {
--- manufacturing_budget/models/mrp_bom_inherit.py
+++ manufacturing_budget/models/mrp_bom_inherit.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 import logging
 
 from odoo import _, api, fields, models
@@ -23,8 +24,63 @@ class MrpBomInherit(models.Model):
     )
     final_data = fields.Text(string="Selected Budget Plan")
 
+    # def action_budget_plan(self):
+    #     """เปิด Wizard Budget Plan"""
+    #     self.ensure_one()
+    #     print("test ====>", self.final_data)
+    #     wizard_values = {
+    #         "final_data": self.final_data,
+    #         "template_id": self.template_id.id if self.template_id else False,
+    #         "fund_analytic_id": (
+    #             self.fund_analytic_id.id if self.fund_analytic_id else False
+    #         ),
+    #     }
+    #     wizard = self.env["budget.plan.wizard"].create(wizard_values)
+
+    #     return {
+    #         "type": "ir.actions.act_window",
+    #         "name": "Budget Plan",
+    #         "res_model": "budget.plan.wizard",
+    #         "view_mode": "form",
+    #         "res_id": wizard.id,
+    #         "target": "new",
+    #         "context": {
+    #             "default_final_data": wizard.final_data,
+    #             "default_template_id": wizard.template_id.id,
+    #             "default_fund_analytic_id": wizard.fund_analytic_id.id,
+    #         },
+    #     }
+
     def action_budget_plan(self):
         """เปิด Wizard Budget Plan"""
+        # self.ensure_one()
+        # final_data_dict = json.loads(self.final_data) if self.final_data else {}
+
+        # template_id = final_data_dict.get("template_id", False)
+        # fund_analytic_id = final_data_dict.get("fund_analytic_id", False)
+        # lines = final_data_dict.get("lines", [])
+        # wizard = self.env["budget.plan.wizard"].search(
+        #     [("mrp_bom_id", "=", self.id)], limit=1
+        # )
+        # print("wizard ===>", wizard)
+        # if final_data_dict:
+        #     wizard_values = {
+        #         "template_id": template_id,
+        #         "fund_analytic_id": fund_analytic_id,
+        #         "wizard_line_ids": [
+        #             (
+        #                 0,
+        #                 0,
+        #                 {
+        #                     "template_line_id": line["template_line_id"],
+        #                     "amount": line.get("amount", 0.0),
+        #                 },
+        #             )
+        #             for line in lines
+        #         ],
+        #     }
+
+        #     wizard = self.env["budget.plan.wizard"].create(wizard_values)
         return {
             "type": "ir.actions.act_window",
             "name": "Budget Plan",
@@ -35,3 +91,14 @@ class MrpBomInherit(models.Model):
                 "default_mrp_bom_id": self.id,
             },
         }
+        # return {
+        #     "type": "ir.actions.act_window",
+        #     "name": "Budget Plan",
+        #     "res_model": "budget.plan.wizard",
+        #     "view_mode": "form",
+        #     "res_id": wizard.id,
+        #     "target": "new",
+        #     "context": {
+        #         "default_mrp_bom_id": self.id,
+        #     },
+        # }
